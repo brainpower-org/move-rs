af044e85d1d0cb4ecf74d26e2ae1f79aff751351
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2018-08-30 18:17:29 +0200

    feat: add client to requets and create data in db

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 #![feature(plugin)]
+#![feature(custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate futures;
 extern crate rocket;
 extern crate rusoto_core;
-extern crate rusoto_credential;
 extern crate rusoto_dynamodb;
 
-use futures::future::Future;
+use rocket::request::{Form, FromForm};
 use rocket::response::NamedFile;
-use rusoto_core::request::HttpClient;
+use rocket::State;
 use rusoto_core::Region;
-use rusoto_credential::{EnvironmentProvider, ProvideAwsCredentials};
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput, ScanInput};
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, PutItemInput, ScanInput};
+use std::collections::HashMap;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+
+#[derive(FromForm)]
+struct Employee {
+    id: String,
+    name: String,
+}
 
 #[get("/area/<area..>")]
 fn area(area: PathBuf) -> String {
@@ -50,46 +56,69 @@ fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
 
+#[get("/tables")]
+fn list_tables(client: State<DynamoDbClient>) -> String {
+    let mut scan_input = ScanInput::default();
+    scan_input.table_name = String::from("rust-skillgroup");
+
+    match client.scan(scan_input).sync() {
+        Ok(scan_output) => format!("{:?}", scan_output),
+        Err(scan_error) => format!("{:?}", scan_error),
+    }
+}
+
+/**
+ * Call with curl
+ * 
+ * curl -X POST \
+  http://localhost:8000/employee \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'id=2&name=rust-update'
+ */
+
+#[post("/employee", data = "<employee>")]
+fn put_employee(client: State<DynamoDbClient>, employee: Form<Employee>) -> String {
+    let mut put_employee = PutItemInput::default();
+    let mut employee_id = AttributeValue::default();
+    let mut employee_name = AttributeValue::default();
+    let employee_data = employee.into_inner();
+
+    employee_id.s = Some(employee_data.id);
+    employee_name.s = Some(employee_data.name);
+
+    put_employee.table_name = String::from("rust-skillgroup");
+    put_employee.item = HashMap::new();
+    put_employee.item.insert(String::from("id"), employee_id);
+    put_employee
+        .item
+        .insert(String::from("name"), employee_name);
+
+    match client.put_item(put_employee).sync() {
+        Ok(scan_output) => format!("{:?}", scan_output),
+        Err(scan_error) => format!("{:?}", scan_error),
+    }
+}
+
 #[get("/<file..>", rank = 1)]
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 fn main() {
-    let creds = EnvironmentProvider.credentials().wait().unwrap();
-    //println!("{:?}", creds);
-    let client = DynamoDbClient::simple(Region::UsEast2);
-
-    let list_tables_input: ListTablesInput = Default::default();
-
-    match client.list_tables(&list_tables_input).sync() {
-        Ok(output) => match output.table_names {
-            Some(table_name_list) => {
-                println!("Tables in database:");
-
-                for table_name in table_name_list {
-                    println!("{}", table_name);
-                    let mut scan_input = ScanInput::default();
-                    scan_input.table_name = String::from("Testtable");
-
-                    match client.scan(&scan_input).sync() {
-                        Ok(scan_output) => {
-                            println!("{:?}", scan_output);
-                        }
-                        Err(scan_error) => {
-                            println!("{:?}", scan_error);
-                        }
-                    };
-                }
-            }
-            None => println!("No tables in database!"),
-        },
-        Err(error) => {
-            println!("Error: {:?}", error);
-        }
-    };
+    let client = DynamoDbClient::new(Region::EuCentral1);
 
     rocket::ignite()
-        .mount("/", routes![area, resource, put_resource, index, files])
+        .mount(
+            "/",
+            routes![
+                area,
+                resource,
+                put_resource,
+                index,
+                list_tables,
+                put_employee,
+                files
+            ],
+        ).manage(client)
         .launch();
 }
