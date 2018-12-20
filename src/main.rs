7968eb76ed813c4f7b9f23b3906320a8e3cfd2b0
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2018-12-20 18:00:25 +0100

    WIP currently valid_keys evaluation is broken

--- src/main.rs
+++ src/main.rs
@@ -44,16 +44,17 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 fn main() {
     // If env vars set and .env is present: fail fast (and tell what to do)
     // let has_relevant_vars = ...;
-    let var_config = MoveConfig::from_vars();
+    let config = MoveConfig::from_vars();
 
     let env_result = match dotenv() {
-        Ok(r) => match var_config {
-            Err(ref config) if config.is_empty() => Ok(format!("using env vars from: {:?}", r)),
-            _ => Err(format!(
-                "Stupid! either set all env vars, or define a complete .env file: {:?}",
-                var_config
-            )),
-        },
+        Ok(ref r) if config.is_empty() => Ok(format!("using env vars from: {:?}", r)),
+        Ok(ref r) => Err(format!(
+            "Mixing env vars and .env file is not supported. You have two options \n \
+             1. Delete file: {:?} \n \
+             2. Unset env vars {:?}",
+            r,
+            config.valid_keys()
+        )),
         Err(dotenv::Error::Io(e)) => Err(format!("{}", e)),
         Err(dotenv::Error::LineParse(key)) => Err(format!("found invalid line in .env: {:?}", key)),
         Err(dotenv::Error::EnvVar(key)) => Err(format!("error: {:?}", key)),
@@ -85,19 +86,35 @@ struct MoveConfig {
 }
 
 impl MoveConfig {
-    fn from_vars() -> Result<MoveConfig, MoveConfig> {
+    fn from_vars() -> MoveConfig {
         let mut config = MoveConfig::new();
         config.id.value = std::env::var(&config.id.name);
         config.key.value = std::env::var(&config.key.name);
 
-        if !config.is_empty() {
-            return Ok(config);
-        }
-        Err(config)
+        config
     }
 
     pub fn is_empty(&self) -> bool {
         self.id.value.is_err() && self.key.value.is_err()
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.id.value.is_ok() && self.key.value.is_ok()
+    }
+
+    pub fn valid_keys(&self) -> Vec<String> {
+        let that = self.clone();
+        let valid_keys = vec![that.clone().id, that.clone().key];
+        valid_keys
+            .iter()
+            .filter_map(|key| { 
+                if key.value.is_ok() {
+                    Some(key.name)
+                } else {
+                    None
+                }
+            })
+            .collect()
     }
 
     fn new() -> Self {
