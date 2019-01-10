946af78836fb5aef2005322d910e634224efc9db
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-01-10 16:28:34 +0100

    add cli with tests

---
+++ tests/integration.rs
@@ -0,0 +1,16 @@
+extern crate assert_cli;
+
+#[cfg(test)]
+mod integration {
+    use assert_cli;
+
+
+    #[test]
+    fn with_hello() {
+        assert_cli::Assert::main_binary()
+            .with_args(&["-p"])
+            .stdout()
+            .contains("Hello, world")
+            .unwrap();
+    }
+}
